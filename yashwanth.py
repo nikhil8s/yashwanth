f0fa077addfa3f348882fd1305ecca3c5469ca68
nikhil8s <163515246+nikhil8s@users.noreply.github.com>
2024-08-05 09:13:48 +0530

    Add files via upload

---
+++ yashwanth.py
@@ -0,0 +1,9 @@
+pria=int(input('enter the age of pria='))
+van=int(input('enter the age of van='))
+man=int(input('enter the age of man='))
+if pria < van:
+ print('van is young of three')
+ if van < man:
+     print('man is young of three')
+     if man < pria:
+         print('pria is young of three')
